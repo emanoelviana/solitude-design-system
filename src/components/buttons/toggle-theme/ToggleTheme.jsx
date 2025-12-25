8a6562879bbb75831ece242fe1d057db0d3a78fb
Emanoel Viana <emanoelvianads@gmail.com>
2025-12-25 06:17:36 -0300

    removing theme context

--- src/components/buttons/toggle-theme/ToggleTheme.jsx
+++ src/components/buttons/toggle-theme/ToggleTheme.jsx
@@ -1,19 +1,14 @@
-import { useTheme } from "../../../contexts/theme/useTheme.jsx";
 import IconButton from "../icon-button/IconButton";
 
 function ToggleTheme({ size = "md" }) {
-  const { theme, setTheme } = useTheme();
-
   return (
     <IconButton
-      icon={theme === "dark" ? "moon-line" : "sun-line"}
+      icon={"sun-line"}
       size={size}
       iconSize={24}
       variant={"filled"}
-      onClick={() => setTheme(theme === "dark" ? "light" : "dark")}
-      title={
-        theme === "dark" ? "Change theme to Light" : "Change theme to Dark"
-      }
+      onClick={() => {}}
+      title={"z"}
     />
   );
 }
