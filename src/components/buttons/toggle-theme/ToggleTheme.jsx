b47b674022971b612fbfa1e03865ebc22461b37b
Emanoel Viana <emanoelvianads@gmail.com>
2025-12-25 05:49:43 -0300

    fix on UseTheme: import correction

--- src/components/buttons/toggle-theme/ToggleTheme.jsx
+++ src/components/buttons/toggle-theme/ToggleTheme.jsx
@@ -1,4 +1,4 @@
-import { useTheme } from "../../../contexts/theme/UseTheme";
+import { useTheme } from "../../../contexts/theme/UseTheme.jsx";
 import IconButton from "../icon-button/IconButton";
 
 function ToggleTheme({ size = "md" }) {
