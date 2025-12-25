628b23c83bf209562c2a78279e33e18f8909eae8
Emanoel Viana <emanoelvianads@gmail.com>
2025-12-25 05:44:29 -0300

    fix on UseTheme: name correction

--- src/contexts/theme/useTheme.jsx
+++ src/contexts/theme/useTheme.jsx
@@ -1,4 +1,4 @@
-import { createContext, useContext, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 
 export function useTheme() {
   const [theme, setTheme] = useState(
