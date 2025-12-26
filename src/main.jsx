d55e4d7732282f761af25d95279d53047c252df4
Emanoel Viana <emanoelvianads@gmail.com>
2025-12-25 22:21:10 -0300

    adding vercel metrics analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { routes } from "./routes/routes";
+import { Analytics } from "@vercel/analytics/react";
 import "./styles/main.css";
 
 createRoot(document.getElementById("root")).render(
-  <RouterProvider router={routes} />
+  <>
+    <RouterProvider router={routes} />
+    <Analytics />
+  </>
 );
